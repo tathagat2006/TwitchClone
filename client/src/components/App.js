161bc5a522188c44af4bd5e58b2fb41598a14008
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-03-19 12:07:54 +0530

    setting up redux router
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const PageOne = () => {
+  return <div>Page 1</div>;
+};
+
+const PageTwo = () => {
+  return <div>Page 2</div>;
+};
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={PageOne} />
+          <Route path="/pagetwo" component={PageTwo} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
