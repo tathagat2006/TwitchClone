09a0675fed88119dcb36dc004b074ac02fc7492a
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-03-21 03:21:55 +0530

    rendering auth status on the screen
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,17 +1,37 @@
 import React from "react";
 
 class GoogleAuth extends React.Component {
+  state = {
+    isSignedIn: null
+  };
+
   componentDidMount() {
     window.gapi.load("client:auth2", () => {
-      window.gapi.client.init({
-        clientID:
-          "703923137606-ceorppgmtv3h7e61dhlru2b1g5pd50ds.apps.googleusercontent.com",
-        scope: "email"
-      });
+      window.gapi.client
+        .init({
+          clientID:
+            "703923137606-ceorppgmtv3h7e61dhlru2b1g5pd50ds.apps.googleusercontent.com",
+          scope: "email"
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        });
     });
   }
+
+  renderAuthButton = () => {
+    if (this.state.isSignedIn === null) {
+      return <div>I don't know if we are signed in?</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in!!</div>;
+    } else {
+      return <div>I am not signed in!!</div>;
+    }
+  };
+
   render() {
-    return <div>Google Auth</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
