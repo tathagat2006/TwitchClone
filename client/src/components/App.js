715e75245cc9b75da7f29669eb728b9ef31be57d
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-03-21 02:29:32 +0530

    error says do not use Link tag outside the Router
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,10 +5,12 @@ import StreamEdit from "./streams/StreamEdit";
 import StreamDelete from "./streams/StreamDelete";
 import StreamList from "./streams/StreamList";
 import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
+      <Header />
       <BrowserRouter>
         <div>
           <Route path="/" exact component={StreamList} />
