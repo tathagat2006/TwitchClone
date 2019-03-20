bfe3c3cae012a16a1a3c172056b0165f4a009064
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-03-21 03:02:11 +0530

    initialising the Oauth library
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,6 +1,15 @@
 import React from "react";
 
 class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client.init({
+        clientID:
+          "703923137606-ceorppgmtv3h7e61dhlru2b1g5pd50ds.apps.googleusercontent.com",
+        scope: "email"
+      });
+    });
+  }
   render() {
     return <div>Google Auth</div>;
   }
