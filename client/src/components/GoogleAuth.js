d8efa29865b7b92fd92fa5bdb6dea5055e163b82
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-03-21 04:08:39 +0530

    connecting the action creator with the auth component
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { connect } from "react-redux";
+import { signIn, signOut } from "../actions";
 
 class GoogleAuth extends React.Component {
   state = {
@@ -21,8 +23,13 @@ class GoogleAuth extends React.Component {
     });
   }
 
-  onAuthChange = () => {
-    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  onAuthChange = isSignedIn => {
+    // this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+    if (isSignedIn) {
+      this.props.signIn();
+    } else {
+      this.props.signOut();
+    }
   };
 
   onSignInClick = () => {
@@ -58,4 +65,7 @@ class GoogleAuth extends React.Component {
   }
 }
 
-export default GoogleAuth;
+export default connect(
+  null,
+  { signIn, signOut }
+)(GoogleAuth);
