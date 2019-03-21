fd6bd5a120835eaa917ad657c8f0bb74962b3bf8
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-03-21 19:27:23 +0530

    automatically handling events
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -2,12 +2,21 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
+  renderInput = formProps => {
+    // console.log(formProps);
+    return (
+      <input
+        onChange={formProps.input.onChange}
+        value={formProps.input.value}
+      />
+    );
+  };
   render() {
     // console.log(this.props);
     return (
       <form>
-        <Field name="title" />
-        <Field name="description" />
+        <Field name="title" component={this.renderInput} />
+        <Field name="description" component={this.renderInput} />
       </form>
     );
   }
