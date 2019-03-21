b020a2e95b5d5ed6118899f3de5cdb2926bd7a38
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-03-21 19:33:08 +0530

    customizing form fields
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -5,18 +5,25 @@ class StreamCreate extends React.Component {
   renderInput = formProps => {
     // console.log(formProps);
     return (
-      <input
-        onChange={formProps.input.onChange}
-        value={formProps.input.value}
-      />
+      <div className="field">
+        <label>{formProps.label}</label>
+        <input
+          onChange={formProps.input.onChange}
+          value={formProps.input.value}
+        />
+      </div>
     );
   };
   render() {
     // console.log(this.props);
     return (
-      <form>
-        <Field name="title" component={this.renderInput} />
-        <Field name="description" component={this.renderInput} />
+      <form className="ui form">
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter Description"
+        />
       </form>
     );
   }
