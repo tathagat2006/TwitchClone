5ecf28356c2623ae4a00baafbb2c522682a0fca7
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-03-21 19:18:31 +0530

    creating forms
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
+class StreamCreate extends React.Component {
+  render() {
+    // console.log(this.props);
+    return (
+      <form>
+        <Field name="title" />
+        <Field name="description" />
+      </form>
+    );
+  }
+}
 
-export default StreamCreate;
+export default reduxForm({
+  form: "streamCreate"
+})(StreamCreate);
