e4cc580b10a7d664e5a4d1c7983441239f24fd48
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-03-22 01:36:56 +0530

    dispatching action after stream creation
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -15,6 +15,7 @@ export const signOut = () => {
 
 export const createStream = formValues => {
   return async dispatch => {
-    streams.post("/streams", formValues);
+    const response = await streams.post("/streams", formValues);
+    dispatch({ type: "CREATE_STREAM", payload: response.data });
   };
 };
