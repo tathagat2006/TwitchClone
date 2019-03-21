794f81f0ef5c337c288f10ed847ebc6de5e5e35c
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-03-22 02:36:35 +0530

    writing fetch streams action creator
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -19,3 +19,8 @@ export const createStream = formValues => {
     dispatch({ type: "CREATE_STREAM", payload: response.data });
   };
 };
+
+export const fetchStreams = () => async dispatch => {
+  const response = await streams.get("/streams");
+  dispatch({ type: "FETCH_STREAMS", payload: response.data });
+};
