492a9b81b991b5e16291bbb0110967fa0154c872
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-03-23 00:51:01 +0530

    merging list records into state
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -2,6 +2,8 @@ import _ from "lodash";
 
 export default (state = {}, action) => {
   switch (action.type) {
+    case "FETCH_STREAMS":
+      return { ...state, ..._.mapKeys(action.payload, "id") };
     case "FETCH_STREAM":
       return { ...state, [action.payload.id]: action.payload };
     case "EDIT_STREAM":
