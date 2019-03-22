e937085fcfee71d9701349704372a43ee60baa6e
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-03-22 17:25:10 +0530

    writing reducers
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

---
+++ client/src/reducers/streamReducer.js
@@ -0,0 +1,12 @@
+export default (state = {}, action) => {
+  switch (action.type) {
+    case "FETCH_STREAM":
+      return { ...state, [action.payload.id]: action.payload };
+    case "EDIT_STREAM":
+      return { ...state, [action.payload.id]: action.payload };
+    case "CREATE_STREAM":
+      return { ...state, [action.payload.id]: action.payload };
+    default:
+      return state;
+  }
+};
