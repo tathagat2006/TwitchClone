865bade030d2306a1ef248a35abce74150da0946
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-03-23 00:58:16 +0530

    fetching the list of all the streams
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { connect } from "react-redux";
+import { fetchStreams } from "../../actions";
 
-const StreamList = () => {
-  return <div>StreamList</div>;
-};
+class StreamList extends React.Component {
+  componentDidMount() {
+    this.props.fetchStreams();
+  }
+  render() {
+    return <div>StreamList</div>;
+  }
+}
 
-export default StreamList;
+export default connect(
+  null,
+  { fetchStreams }
+)(StreamList);
