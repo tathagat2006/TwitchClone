96c6ca0aa147c309db5c48c7c26d11dc6afdafbe
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-03-24 01:47:10 +0530

    fetching stream for edit stream bug fixed
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,14 +1,26 @@
 import React from "react";
 import { connect } from "react-redux";
+import { fetchStream } from "../../actions";
 
-const StreamEdit = props => {
-  console.log(props);
-  return <div>{this.props}</div>;
-};
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+  render() {
+    console.log(this.props);
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+    return <div>{this.props.stream.title}</div>;
+  }
+}
 
 const mapStateToProps = (state, ownProps) => {
   // console.log(ownProps);
   return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps)(StreamEdit);
+export default connect(
+  mapStateToProps,
+  { fetchStream }
+)(StreamEdit);
