4842f3f1cf17549ab9189f3bfb0a5bddcfcbac56
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-05-29 23:55:22 +0530

    adding ondismiss prop to modal component
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Modal from "../Modal";
+import history from "../../history";
 
 const StreamDelete = () => {
   const actions = (
-    <div>
+    <React.Fragment>
       <button className="ui button negative">Delete</button>
       <button className="ui button">Cancel</button>
-    </div>
+    </React.Fragment>
   );
   return (
     <div>
@@ -15,6 +16,7 @@ const StreamDelete = () => {
         title="Delete Stream"
         content="Are you sure you want to delete the stream?"
         actions={actions}
+        onDismiss={() => history.push("/")}
       />
     </div>
   );
